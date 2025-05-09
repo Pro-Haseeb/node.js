58216fde0251543c8f866e63c0b9d29f7ea9551a
Haseeb Mushtaq <haseebmushtaq874@gmail.com>
2025-05-09 11:25:23 -0700

    Event Module Added

--- eventModule.mjs
+++ eventModule.mjs
@@ -3,7 +3,12 @@ import { EventEmitter } from 'node:events';
 class MyEmitter extends EventEmitter {}
 
 const myEmitter = new MyEmitter();
-myEmitter.on('event', () => {
-  console.log('an event occurred!');
+myEmitter.on('war', () => {
+  console.log('India na Rocket hamla kiya Pakistan pr');
+  setTimeout(() => {
+    console.log("Pakistan na India ka 5 Tiyyara gira diya");
+  }, 3000);
 });
-myEmitter.emit('event');
+console.log("Every one is living peacefully : -)");
+
+myEmitter.emit('war');
